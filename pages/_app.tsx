01a287bb2c20da0f558aa15ccb0e8abcd6f081a9
AaronDennis07 <aarondennisbv01@gmail.com>
2022-10-25 13:33:38 +0530

    first commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,22 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import Script from "next/script"
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+
+
+  return (
+    <>
+      <Script id="Adsense-id"  crossOrigin="anonymous" data-ad-client="ca-pub-6813609620822362" async strategy="afterInteractive" onError={(e)=>{console.error(e)}}
+    src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"
+  />
+  <Component {...pageProps} />
+    </>
+  )
+
+  
 }
 
 export default MyApp
+
+
